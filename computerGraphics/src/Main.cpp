cd8a5a21c3d643f2a407677be0e32bf49ea4575b
kafkaphoenix <jaguilerapuerta@gmail.com>
2017-06-09 23:09:16 +0200

    Vec3Class dot, cross product, length, normalize

---
+++ computerGraphics/src/Main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 9 jun. 2017
+ *      Author: sorak
+ */
+#include <iostream>
+#include "Vec3.h"
+#include <String>
+
+int main(int argc, char **argv)
+{
+	typedef Vec3 <float> Vec3f;
+
+	Vec3f a;
+
+	return 0;
+}
+
+
